83ef5419261bfc117df152389a0132835f82288d
unknown <none070777@gmail.com>
2024-07-11 21:41:22 +0500

    save

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,11 @@ import cors from "cors"
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.json())
 app.use("/api", router)
 app.use(errorMiddleware);
-app.use(cors())
+
 
 const start = async () => {
     try {
